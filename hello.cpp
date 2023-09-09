2b8cd41157d53e4ba0d279e8a6e87d9c0b2e6832
James Elliott <jameselliott@JAMES-L.local>
2023-09-09 19:38:59 +1200

    initial

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+/* 
+Directives (#) are read and interpreted by the preprocessor.
+<iostream> includes a section of the standard C++ code known as a header iostream.
+This allows the program to perform standard input and output operations.
+ */
+
+int main()
+{
+    std::cout << "Hello,C++\n"; // Hello, C++
+    std::cout << "It's nice to meet you~"; // It's nice to meet you~
+}
