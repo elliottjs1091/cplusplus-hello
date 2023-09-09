f1a367908ce86bb3174066cebdcb4ba9dbaee60a
James Elliott <jameselliott@JAMES-L.local>
2023-09-09 20:07:40 +1200

    add: instructions for c++ install and program creation

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,11 @@
+/*
+In MacOS zsh
+1: install homebrew (don't copy $)
+    $ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+2: install clang
+    $ brew install clang
+*/
+
 #include <iostream> 
 /* 
 Directives (#) are read and interpreted by the preprocessor.
@@ -10,3 +18,10 @@ int main()
     std::cout << "Hello,C++\n"; // Hello, C++
     std::cout << "It's nice to meet you~"; // It's nice to meet you~
 }
+
+/*
+1: compile the cpp file into an executable in a shell with 
+    $ clang++ hello.cpp -o <executable-file-name>
+2: run the executable
+3: good job! you now have your first c++ program on MacOS
+*/
